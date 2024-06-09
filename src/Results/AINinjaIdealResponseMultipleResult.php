076c74489b734b1b3e192e4f956f0b296ef9c777
Ian Rothmann <ian@conceptsphere.com>
2024-06-09 13:19:58 +0400

    Getting questions for multiple ideal response

--- src/Results/AINinjaIdealResponseMultipleResult.php
+++ src/Results/AINinjaIdealResponseMultipleResult.php
@@ -2,6 +2,12 @@
 
 namespace IanRothmann\AINinja\Results;
 
+use Illuminate\Support\Collection;
+
 class AINinjaIdealResponseMultipleResult extends AINinjaResult
 {
+    public function getQuestions(): Collection
+    {
+        return collect(collect($this->result)->get('questions'));
+    }
 }
