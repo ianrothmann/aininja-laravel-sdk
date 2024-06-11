3b8262c984f17e9c879b117a767dc115bda13e76
Ian Rothmann <ian@conceptsphere.com>
2024-06-11 11:05:23 +0400

    Question chat navigator updates

--- src/Results/AINinjaQuestionChatNavigatorResult.php
+++ src/Results/AINinjaQuestionChatNavigatorResult.php
@@ -9,6 +9,31 @@ class AINinjaQuestionChatNavigatorResult extends AINinjaResult
         return $this->result['type'] ?? null;
     }
 
+    public function intentWasAnswerProvided(): bool
+    {
+        return $this->getIntent() === 'ProvideAnswer';
+    }
+
+    public function intentWasToGoBack(): bool
+    {
+        return $this->getIntent() === 'BackToPreviousQuestion';
+    }
+
+    public function intentWasThatClarificationIsRequired(): bool
+    {
+        return $this->getIntent() === 'ClarifyQuestion';
+    }
+
+    public function intentWasThatTheUserDoesNotKnow(): bool
+    {
+        return $this->getIntent() === 'DontKnowAnswer';
+    }
+
+    public function intentWasToContinueToNextQuestion(): bool
+    {
+        return $this->getIntent() === 'NextQuestion';
+    }
+
     public function getQuestion(): ?string
     {
         return $this->result['args']['question'] ?? null;
@@ -19,10 +44,10 @@ class AINinjaQuestionChatNavigatorResult extends AINinjaResult
         return $this->result['args']['question_number'] ?? null;
     }
 
-    public function getComplement(): ?string
+   /* public function getComplement(): ?string
     {
         return $this->result['args']['complement'] ?? null;
-    }
+    }*/
 
     public function getComment(): ?string
     {
