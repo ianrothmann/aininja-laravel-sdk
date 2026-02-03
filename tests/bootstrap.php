d8330a0b5ab8d99d5769d0fd98066744016025d2
ianrothmann <4319223+ianrothmann@users.noreply.github.com>
2026-02-03 08:23:20 +0000

    Fix styling

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,6 @@ set_error_handler(function ($severity, $message, $file, $line) {
     if ($severity === E_DEPRECATED || $severity === E_USER_DEPRECATED) {
         return true;
     }
+
     return false;
 }, E_DEPRECATED | E_USER_DEPRECATED);
